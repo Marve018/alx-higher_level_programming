31c0df493472a335db5654931474e3c96eae0ea9
Marve018 <okechukwuchukwuemeka018@gmail.com>
2023-08-13 07:12:11 -0700

    Task 1 fixed bug

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -5,7 +5,5 @@ def element_at(my_list, idx):
             return None
         elif idx >= len(my_list):
             return None
-        elif idx == []:
-            return None
         else:
             return my_list[idx]
