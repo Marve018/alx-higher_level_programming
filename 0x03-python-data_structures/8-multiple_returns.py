313ab92d856dbfb485f7832d150b51055704fc2c
Marve018 <okechukwuchukwuemeka018@gmail.com>
2023-08-14 07:42:50 -0700

    task 8 bug fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,5 +2,5 @@
 def multiple_returns(sentence):
     tuple_n = (len(sentence), sentence[0])
     if len(sentence) == 0:
-        return None
+        return len(sentence), "None"
     return tuple_n
