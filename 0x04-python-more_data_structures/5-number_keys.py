704c706d11b9628f6c7c6441f85f657e15d8363f
Marve018 <okechukwuchukwuemeka018@gmail.com>
2023-08-17 10:04:11 -0700

    task 5

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    key_count = 0  # initializing a counter
+    for i in range(len(a_dictionary)):
+        key_count = i + 1
+    return key_count
