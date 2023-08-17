79596af48055266ed9fe26de672e32db069471b1
Marve018 <okechukwuchukwuemeka018@gmail.com>
2023-08-17 09:46:09 -0700

    task 4 bug fix

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -4,7 +4,7 @@ def only_diff_elements(set_1, set_2):
     for element in set_1:
         if element not in set_2:
             diff_set.add(element)
-        for element in set_2:
+    for element in set_2:
             if element not in set_1:
                 diff_set.add(element)
     return diff_set
