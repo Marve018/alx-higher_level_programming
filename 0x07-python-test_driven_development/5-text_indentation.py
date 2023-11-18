ca652426b021d28351e1cbf0240f32e25ea858fd
Marve018 <122813162+Marve018@users.noreply.github.com>
2023-11-18 19:59:58 +0100

    corrections on the function

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -13,6 +13,6 @@ def text_indentation(text):
     if type(text) != str:
         raise TypeError("text must be a string")
     text1 = text.replace('?', '?\n\n')
-    text1 = text1.replace('.', '?\n\n')
-    text1 = text1.replace(':', '?\n\n')
+    text1 = text1.replace('?', '.\n\n')
+    text1 = text1.replace(':?', ':\n\n')
     print("\n".join([i.strip() for i in text1.split("\n")]), end="")
