4a7ef58f9f071b16781fc95e98438240d977ca01
Marve018 <okechukwuchukwuemeka018@gmail.com>
2024-01-13 13:34:39 +0100

    cleared whitespaces

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,16 +6,16 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    connection = MySQLdb.connect(host='localhost', port=3306, 
+    connection = MySQLdb.connect(host='localhost', port=3306,
                                  user=sys.argv[1],
                                  passwd=sys.argv[2],
                                  db=sys.argv[3])
-    
+
     cur = connection.cursor()
     cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
-    
-    #printing out
+
+    # printing out
     for row in rows:
         print(row)
 
