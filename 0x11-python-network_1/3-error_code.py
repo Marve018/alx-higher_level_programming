7c9dfdb6cd5c5debbb49b79fd39e8958498bbff5
Marve018 <okechukwuchukwuemeka018@gmail.com>
2024-01-26 23:04:14 +0100

    {feat}: Script that manages urllib.error.HTTPError exceptions and print: Error code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Manage urllib.error.HTTPError exceptions and print: Error code:
+"""
+import sys
+from urllib import error, request
+
+
+if __name__ == "__main__":
+    
+    url = request.Request(sys.argv[1])
+    try:
+        with request.urlopen(url) as response:
+            print(response.read().decode("ascii"))
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code))
